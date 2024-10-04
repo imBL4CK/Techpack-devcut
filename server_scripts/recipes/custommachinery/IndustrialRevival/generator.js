20ea47ac246d65d10aa9e273cbddad8de3b09e1a
BL4CK_ <160023261+imBL4CK@users.noreply.github.com>
2024-10-03 22:46:19 -0300

    test

--- server_scripts/recipes/custommachinery/IndustrialRevival/generator.js
+++
@@ -1,6 +0,0 @@
-ServerEvents.recipes((e) => {
-  //Fluid Tank
-  e.recipes.custommachinery.custom_machine("custommachinery:industrialrevival/generator", 600)
-    .produceEnergyPerTick(16)
-    .requireItemTag("minecraft:coals");
-});
--- startup_scripts/registry/ores.js
+++
@@ -1,6 +0,0 @@
-StartupEvents.registry("item", (e) => {
-  let item = [];
-  item.forEach((registry) => {
-    e.create(registry + "_ingot");
-  });
-});
