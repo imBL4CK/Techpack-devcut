50a03b8fbf32831e0cfd50562f23bc1de5429817
BL4CK_ <160023261+imBL4CK@users.noreply.github.com>
2025-02-21 10:50:41 -0300

    - Fixed Hephaestus Forge Recipe Format - Added Elder Miner's Pickaxe Tool Tier - Added Elder Miner's Pickaxe Recipe

--- kubejs/server_scripts/recipes/forbiddena_arcanus/hephaestus_forge.js
+++ kubejs/server_scripts/recipes/forbiddena_arcanus/hephaestus_forge.js
@@ -1,41 +1,76 @@
 ServerEvents.lowPriorityData(e => {
-    //Mana Pool
-    event.addJson('forbidden_arcanus:forbidden_arcanus/hephaestus_forge/ritual/mana_pool', {
-        "additional_requirements": {
-            "forge_tier": 1
+
+    const recipes = [
+    //Manapool
+      {
+        id: "manapool",
+        tier: 1,
+        essences: {
+          aureal: 100,
+          blood: 0,
+          experience:1000,
+          souls:3,
         },
-        "essences": {
-            "aureal": 100,
-            "blood": 0,
-            "experience": 1000,
-            "souls": 3
+        inputs: {
+          pedestals:[
+          {
+              "amount": 3,
+              "ingredient": {
+                  "item": "botania:livingrock"
+              }
+          },
+          {
+              "amount": 1,
+              "ingredient": {
+                  "item": "forbidden_arcanus:carved_edelwood_log"
+              }
+          }],
+          mainItem: "eidolon:holy_symbol"
         },
-        "inputs": [
-            {
-                "amount": 3,
-                "ingredient": {
-                    "item": "botania:livingrock"
-                }
-            },
-            {
-                "amount": 1,
-                "ingredient": {
-                    "item": "forbidden_arcanus:carved_edelwood_log"
-                }
-            }
-        ],
-        "main_ingredient": {
-            "item": "eidolon:holy_symbol"
-        },
-        "result": {
-            "type": "forbidden_arcanus:create_item",
-            "result_item": {
-                "Count": 1,
-                "id": "botania:mana_pool"
-            }
+        outputs: {
+          quantity: 1,
+          item: "botania:manapool"
         }
-    })
-    const recipes = []
+      },
+    //Elder Miner's Pickaxe
+    {
+        id: "elders_miner_pickaxe",
+        tier: 3,
+        essences: {
+        aureal: 100,
+        blood: 0,
+        experience:1000,
+        souls:15,
+        },
+        inputs: {
+        pedestals:[
+        {
+            "amount": 3,
+            "ingredient": {
+                "item": "botania:manasteel_ingot"
+            }
+        },
+        {
+            "amount": 3,
+            "ingredient": {
+                "item": "botania:elementium_ingot"
+            }
+        },
+        {
+            "amount": 3,
+            "ingredient": {
+                "item": "malum:maligant_pewter_ingot"
+            }
+        },
+    ],
+        mainItem: "eidolon:holy_symbol"
+        },
+        outputs: {
+        quantity: 1,
+        item: "kubejs:elders_miner_pickaxe"
+        }
+    }
+    ]
 
     recipes.forEach((recipe) => {
 
@@ -47,30 +82,17 @@ ServerEvents.lowPriorityData(e => {
                 "aureal": recipe.essences.aureal,
                 "blood": recipe.essences.blood,
                 "experience": recipe.essences.experience,
-                "souls": 3
+                "souls": recipe.essences.souls
             },
-            "inputs": [
-                {
-                    "amount": 3,
-                    "ingredient": {
-                        "item": "botania:livingrock"
-                    }
-                },
-                {
-                    "amount": 1,
-                    "ingredient": {
-                        "item": "forbidden_arcanus:carved_edelwood_log"
-                    }
-                }
-            ],
+            "inputs": recipe.inputs.pedestals,
             "main_ingredient": {
-                "item": "eidolon:holy_symbol"
+                "item": recipe.inputs.mainItem
             },
             "result": {
                 "type": "forbidden_arcanus:create_item",
                 "result_item": {
-                    "Count": 1,
-                    "id": "botania:mana_pool"
+                    "Count": recipe.outputs.quantity,
+                    "id": recipe.outputs.item
                 }
             }
         })
